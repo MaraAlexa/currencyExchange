744358581a3412a672957a2d66d3d65f35e119d3
MaraAlexa <maraalexa57@gmail.com>
2015-11-27 22:29:01 +0100

    new title/basic 2 way binding concept

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-angular.module("currencyExchange", []);
+
+angular.module("currencyExchange", [])
+.controller('CurrencyCalculatorController', function ($scope) {
+
+    $scope.currencies = [
+      {id: 1, name:'euro', select: 'false'},
+      {id: 2, name:'gbp', select: 'false'},
+      {id: 3, name:'usd', select: 'false'},
+      {id: 4, name:'yen', select: 'false'}
+    ];
+
+    $scope.selectedCurrency = "euro";
+    $scope.selectedCurrencyOutput = "usd";
+
+
+    $scope.selectedCurrencyIs = function (item) {
+
+        $scope.selectedCurrency = item;
+      
+
+
+    };
+
+
+});
